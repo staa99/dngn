691ecf47cc6b5f8e20daef3cc9c6760e9d932927
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2021-12-27 06:38:03 +0100

    Configure linter and apply style rules

--- dashboard/pages/_app.tsx
+++ dashboard/pages/_app.tsx
@@ -1,12 +1,12 @@
-import {ChakraProvider} from '@chakra-ui/react'
-import type {AppProps} from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from 'next/app'
 
-function App({Component, pageProps}: AppProps) {
-    return (
-        <ChakraProvider>
-            <Component {...pageProps} />
-        </ChakraProvider>
-    )
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default App
--- dashboard/pages/index.tsx
+++ dashboard/pages/index.tsx
@@ -1,13 +1,13 @@
-import {Container, Flex} from '@chakra-ui/react'
+import { Container, Flex } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-      <Container maxWidth='container.xl' padding={0}>
-          <Flex height='100vh' paddingY={20}>
-              <h1>Hello Next.js</h1>
-          </Flex>
-      </Container>
+    <Container maxWidth="container.xl" padding={0}>
+      <Flex height="100vh" paddingY={20}>
+        <h1>Hello Next.js</h1>
+      </Flex>
+    </Container>
   )
 }
 
