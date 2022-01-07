346a0a8b5287872c5034528fb8aa24fbf56ef67b
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-07 03:42:08 +0100

    Implement RSA decryption

--- minter/src/crypto-extensions/index.ts
+++ minter/src/crypto-extensions/index.ts
@@ -1,17 +1,19 @@
-export function rsaEncryptJson(
-  data: Record<any, unknown>,
-  publicKey: string
-): Buffer {
-  const jsonString = JSON.stringify(data)
-  // todo: Implement encryption here
-  return Buffer.from(jsonString)
-}
+import * as crypto from 'crypto'
 
 export function rsaDecryptJson<ResultType>(
   encryptedData: Buffer,
   privateKey: string
 ): ResultType {
-  const jsonString = Buffer.from(encryptedData).toString()
-  // todo: Implement decryption here
+  const decryptedData = crypto.privateDecrypt(
+    {
+      key: Buffer.from(privateKey),
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: 'sha256',
+    },
+    encryptedData
+  )
+  const jsonString = Buffer.from(decryptedData).toString()
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return JSON.parse(jsonString)
 }
