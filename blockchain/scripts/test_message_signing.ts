fc783d092db86f533e8dbf5478a5e7f01dce5f21
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-11 08:28:19 +0100

    Add a test script for generating signed messages

---
+++ blockchain/scripts/test_message_signing.ts
@@ -0,0 +1,19 @@
+﻿import { ethers } from 'hardhat'
+import { randomBytes } from 'crypto'
+
+async function main() {
+  const signers = await ethers.getSigners()
+  const signer = signers[0]
+  const message = `REGISTER_CODE:${randomBytes(16).toString('hex')}`
+  const signature = await signer.signMessage(`REGISTER_CODE:${message}`)
+  console.log(
+    `Address: ${signer.address}\nMessage: ${message}\nSignature: ${signature}`
+  )
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
