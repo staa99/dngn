4b20b0fef677e09ae03e6dd2dbba6007a49127a4
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-12 04:55:23 +0100

    Move register model to new file

--- dngn-api-backend/ApiModels/LoginModel.cs
+++ dngn-api-backend/ApiModels/LoginModel.cs
@@ -1,32 +1,13 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace DngnApiBackend.ApiModels
 {
     public class LoginModel
     {
         [Required]
-        public string? Address { get; set; }
-
-        [Required]
-        public string? Signature { get; set; }
-    }
-
-    public class RegisterModel
-    {
-        [Required]
         public string Address { get; set; } = null!;
 
         [Required]
-        public Guid SignedData { get; set; }
-
-        [Required]
         public string Signature { get; set; } = null!;
-
-        [Required]
-        public string FirstName { get; set; } = null!;
-
-        [Required]
-        public string LastName { get; set; } = null!;
     }
 }
---
+++ dngn-api-backend/ApiModels/RegisterModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DngnApiBackend.ApiModels
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        public Guid SignedData { get; set; }
+
+        [Required]
+        public string Signature { get; set; } = null!;
+
+        [Required]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        public string LastName { get; set; } = null!;
+    }
+}
