6aa9fea0e666c8c6da8cccc56879a0cba7ebbdbc
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-12 04:55:49 +0100

    Implement deposit account model

---
+++ dngn-api-backend/ApiModels/GenerateDepositAccountModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DngnApiBackend.ApiModels
+{
+    public class GenerateDepositAccountModel
+    {
+        [Required]
+        public string BVN { get; set; } = null!;
+    }
+}
