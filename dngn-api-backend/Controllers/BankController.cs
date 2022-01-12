c8e8c64e14f6f2aa15630ad76f3715c27872485c
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-12 04:56:50 +0100

    Implement bank controller

---
+++ dngn-api-backend/Controllers/BankController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using DngnApiBackend.Services.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DngnApiBackend.Controllers
+{
+    [Route("banks")]
+    public class BankController : BaseController
+    {
+        private readonly IBankRepository _bankRepository;
+
+        public BankController(IBankRepository bankRepository)
+        {
+            _bankRepository = bankRepository;
+        }
+
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetBanksAsync(string? query)
+        {
+            var banks = await _bankRepository.GetBanksAsync(query);
+            return Ok(new
+            {
+                status  = "success",
+                message = "Banks loaded successfully",
+                data    = banks
+            });
+        }
+    }
+}
