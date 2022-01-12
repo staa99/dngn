79f42e13e3d43bad8977a698daf77920b57be687
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-12 04:56:10 +0100

    Update base controller for middleware error handling

--- dngn-api-backend/Controllers/BaseController.cs
+++ dngn-api-backend/Controllers/BaseController.cs
@@ -1,32 +1,36 @@
-﻿using System.Linq;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using DngnApiBackend.Exceptions;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 
 namespace DngnApiBackend.Controllers
 {
     public class BaseController : Controller
     {
-        protected virtual IActionResult ModelStateErrorResult()
+        protected ObjectId CurrentUserId =>
+            ObjectId.TryParse(User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value, out var id)
+                ? id
+                : throw new UserException("UNAUTHORIZED", "You must be logged in to use this service");
+        
+        protected virtual void AssertValidModelState()
         {
-            return UserError("INVALID_REQUEST_DATA",
-                ModelState.FirstOrDefault().Value?.Errors?.FirstOrDefault()?.ErrorMessage);
+            if (!ModelState.IsValid)
+            {
+                throw new UserException("INVALID_REQUEST_DATA",
+                    ModelState.FirstOrDefault().Value?.Errors?.FirstOrDefault()?.ErrorMessage ??
+                    "The request could not be processed");
+            }
         }
 
-        protected virtual IActionResult UserError(string code, string? message)
+        protected virtual void ThrowUserError(string code, string message)
         {
-            return BadRequest(new
-            {
-                status = "failed",
-                error  = new {code, message}
-            });
+            throw new UserException(code, message);
         }
 
-        protected virtual IActionResult ServiceError(string code, string message)
+        protected virtual void ThrowServiceError(string code, string message)
         {
-            return StatusCode(500, new
-            {
-                status = "failed",
-                error  = new {code, message}
-            });
+            throw new ServiceException(code, message);
         }
     }
 }
