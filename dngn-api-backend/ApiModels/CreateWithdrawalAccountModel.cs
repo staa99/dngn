70797ea4649af9acf99317fa436732cbf3f95208
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-12 04:55:38 +0100

    Implement create withdrawal account model

---
+++ dngn-api-backend/ApiModels/CreateWithdrawalAccountModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DngnApiBackend.ApiModels
+{
+    public class CreateWithdrawalAccountModel
+    {
+        [Required]
+        public string AccountNumber { get; set; } = null!;
+        
+        [Required]
+        public string AccountName { get; set; } = null!;
+        
+        [Required]
+        public string BankId { get; set; } = null!;
+    }
+}
