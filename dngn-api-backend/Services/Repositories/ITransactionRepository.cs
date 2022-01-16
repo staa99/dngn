51e05f84fa80788a2356db7f2e7be431838400e0
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-16 05:51:37 +0100

    Add common interface for transaction repositories

---
+++ dngn-api-backend/Services/Repositories/ITransactionRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using DngnApiBackend.Services.Dto;
+using MongoDB.Bson;
+
+namespace DngnApiBackend.Services.Repositories
+{
+    public interface ITransactionRepository
+    {
+        Task<ObjectId> CreateTransactionAsync<TDto>(TDto dto) where TDto : CreateTransactionDto;
+        Task<TransactionDto?> GetTransactionAsync(ObjectId id);
+        Task<TransactionDto?> GetTransactionByBankReferenceAsync(string bankReference);
+        Task<TransactionDto?> GetTransactionByProviderReferenceAsync(string providerReference);
+        Task<TransactionDto?> GetTransactionByReferenceAsync(Guid transactionReference);
+    }
+}
