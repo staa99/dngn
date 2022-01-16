43660ae5dfeacfba98005be9c5f46b4a6e4b6eca
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-16 05:42:18 +0100

    Add bank account metadata and nullable bank id

--- dngn-api-backend/Data/Models/BankAccount.cs
+++ dngn-api-backend/Data/Models/BankAccount.cs
@@ -1,12 +1,14 @@
-﻿using MongoDB.Bson;
+﻿using System.Collections.Generic;
+using MongoDB.Bson;
 
 namespace DngnApiBackend.Data.Models
 {
     public class BankAccount : BaseModel
     {
         public string AccountNumber { get; set; } = null!;
-        public ObjectId BankId { get; set; }
+        public ObjectId? BankId { get; set; }
         public string AccountName { get; set; } = null!;
         public bool IsVirtual { get; set; }
+        public IDictionary<BankAccountMetaKey, string> Metadata { get; set; } = new Dictionary<BankAccountMetaKey, string>();
     }
 }
