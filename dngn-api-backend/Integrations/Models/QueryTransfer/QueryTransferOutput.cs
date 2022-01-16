23a4071a38c0fd99aa8f9725dbde5d331d3d1112
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-16 05:57:36 +0100

    Implement QueryTransferOutput

---
+++ dngn-api-backend/Integrations/Models/QueryTransfer/QueryTransferOutput.cs
@@ -0,0 +1,8 @@
+using DngnApiBackend.Integrations.Models.Common;
+
+namespace DngnApiBackend.Integrations.Models.QueryTransfer
+{
+    public class QueryTransferOutput : BaseTransferOutput
+    {
+    }
+}
