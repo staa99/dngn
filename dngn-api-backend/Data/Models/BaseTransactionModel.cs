b49b467d75214cdadae42ff24d0678e06645a5ef
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-17 02:33:44 +0100

    Add provider and completion date to transaction model

--- dngn-api-backend/Data/Models/BaseTransactionModel.cs
+++ dngn-api-backend/Data/Models/BaseTransactionModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DngnApiBackend.Integrations.Models.Common;
 using MongoDB.Bson;
 
 namespace DngnApiBackend.Data.Models
@@ -11,9 +12,11 @@ namespace DngnApiBackend.Data.Models
         public long Amount { get; set; }
         public long ProviderFees { get; set; }
         public long TotalPlatformFees { get; set; }
+        public TransactionProvider Provider { get; set; }
         public TransactionStatus Status { get; set; }
         public TransactionType TransactionType { get; set; }
         public ObjectId UserAccountId { get; set; }
         public ObjectId BankAccountId { get; set; }
+        public DateTimeOffset DateCompleted { get; set; }
     }
 }
