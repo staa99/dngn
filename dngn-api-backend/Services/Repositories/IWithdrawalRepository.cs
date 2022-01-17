f71e546efa13e9ef561dac3b9bb09abe390fb098
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-17 02:43:40 +0100

    Implement withdrawal repository

--- dngn-api-backend/Services/Repositories/IWithdrawalRepository.cs
+++ dngn-api-backend/Services/Repositories/IWithdrawalRepository.cs
@@ -1,4 +1,11 @@
-﻿namespace DngnApiBackend.Services.Repositories
+﻿using System.Threading.Tasks;
+using DngnApiBackend.Data.Models;
+using MongoDB.Bson;
+
+namespace DngnApiBackend.Services.Repositories
 {
-    public interface IWithdrawalRepository : ITransactionRepository{}
+    public interface IWithdrawalRepository : ITransactionRepository
+    {
+        Task UpdateTransactionStatusAsync(ObjectId transactionId, TransactionStatus status, long providerFees);
+    }
 }
--- dngn-api-backend/Services/Repositories/WithdrawalRepository.cs
+++ dngn-api-backend/Services/Repositories/WithdrawalRepository.cs
@@ -1,7 +1,11 @@
-﻿using DngnApiBackend.Data.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DngnApiBackend.Data.Models;
 using DngnApiBackend.Exceptions;
 using DngnApiBackend.Services.Dto;
 using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace DngnApiBackend.Services.Repositories
@@ -24,6 +28,26 @@ namespace DngnApiBackend.Services.Repositories
 
             _logger.LogError("Create transaction has been called with the wrong type: {Type}", typeof(TDto));
             throw new ServiceException("INVALID_DATA_TYPE", "The data is not valid for withdrawal transactions");
+        }
+
+        public async Task UpdateTransactionStatusAsync(ObjectId transactionId, TransactionStatus status, long providerFees)
+        {
+            var updates = new List<UpdateDefinition<Withdrawal>>
+            {
+                UpdateBuilder.Set(a => a.Status, status),
+                UpdateBuilder.Set(a => a.ProviderFees, providerFees),
+            };
+            if (status is TransactionStatus.Successful or TransactionStatus.Failed)
+            {
+                updates.Add(UpdateBuilder.Set(a => a.DateCompleted, DateTimeOffset.UtcNow));
+            }
+            var updateDefinition = BuildUpdate(updates);
+
+            var updateResult = await Collection.UpdateOneAsync(FilterById(transactionId), updateDefinition);
+            if (!updateResult.IsAcknowledged || updateResult.ModifiedCount == 0)
+            {
+                throw new UserException("TRANSACTION_NOT_FOUND", "Transaction not found");
+            }
         }
     }
 }
