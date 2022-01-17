4f404a6544aa4bec29b4dae9575ae1b77bf80661
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-17 02:31:21 +0100

    Add strongly typed config types for platform config

---
+++ dngn-api-backend/Configuration/DngnOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace DngnApiBackend.Configuration
+{
+    public class DngnOptions
+    {
+        public decimal CreditChargePercentage { get; set; }
+        public long CreditChargeLimit { get; set; }
+        public decimal DebitChargePercentage { get; set; }
+        public long DebitChargeLimit { get; set; }
+    }
+}
