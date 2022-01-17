5bb0d06f6fdf5d42f3992cded7c29c7f1f97c391
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-17 02:47:16 +0100

    Implement bank transfer hooks

---
+++ dngn-api-backend/Integrations/PaymentProviders/Flutterwave/Services/WebHooks/FlutterwaveInwardBankTransferHook.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Threading.Tasks;
+using DngnApiBackend.Data.Models;
+using DngnApiBackend.Integrations.Models.Common;
+using DngnApiBackend.Integrations.Models.WebHooks;
+using DngnApiBackend.Integrations.VirtualAccounts;
+using DngnApiBackend.Services.Dto;
+using DngnApiBackend.Services.Platform.Pricing;
+using DngnApiBackend.Services.Repositories;
+using DngnApiBackend.Services.WebhookServices;
+using Microsoft.Extensions.Logging;
+
+// ReSharper disable SwitchStatementMissingSomeEnumCasesNoDefault
+
+
+namespace DngnApiBackend.Integrations.PaymentProviders.Flutterwave.Services.WebHooks
+{
+    // todo: Implement customer notifications
+    public class FlutterwaveInwardBankTransferHook : IInwardBankTransferHook
+    {
+        private readonly IBankAccountRepository _bankAccountRepository;
+        private readonly IVirtualAccountCreditTransactionVerifier _creditTransactionVerifier;
+        private readonly IDepositRepository _depositRepository;
+        private readonly ILogger<FlutterwaveInwardBankTransferHook> _logger;
+        private readonly IPriceCalculatorService _priceCalculatorService;
+
+
+        public FlutterwaveInwardBankTransferHook(IDepositRepository depositRepository,
+            IBankAccountRepository bankAccountRepository,
+            IVirtualAccountCreditTransactionVerifier creditTransactionVerifier,
+            IPriceCalculatorService priceCalculatorService,
+            ILogger<FlutterwaveInwardBankTransferHook> logger)
+        {
+            _depositRepository         = depositRepository;
+            _bankAccountRepository     = bankAccountRepository;
+            _creditTransactionVerifier = creditTransactionVerifier;
+            _priceCalculatorService    = priceCalculatorService;
+            _logger                    = logger;
+        }
+
+
+        public async Task ProcessHookAsync(JsonWebhookModel model)
+        {
+            if (model is not FlutterwaveJsonWebhookModel flwModel)
+            {
+                _logger.LogInformation(
+                    "InwardBankTransferHook: Invalid attempt to process a non-flutterwave webhook model by a flutterwave webhook processor");
+
+                return;
+            }
+
+            var providerReference = flwModel.Id;
+            _logger.LogInformation("InwardBankTransferHook: A notification is being processed. Reference: {Reference}",
+                providerReference);
+
+            // load the transaction data from flutterwave
+            var data = await _creditTransactionVerifier.VerifyVirtualAccountCreditTransactionAsync(providerReference);
+            if (data == null)
+            {
+                _logger.LogInformation(
+                    "InwardBankTransferHook: The provider failed to resolve the reference '{Reference}'",
+                    providerReference);
+
+                return;
+            }
+
+            try
+            {
+                if (!data.IsSuccessful)
+                {
+                    _logger.LogError("Transaction failed: {Reference}", providerReference);
+                    return;
+                }
+
+                var newStatus = data.IsSuccessful ? TransactionStatus.Successful : TransactionStatus.Failed;
+                var transaction =
+                    await _depositRepository.GetTransactionByProviderReferenceAsync(TransactionProvider.Flutterwave,
+                        providerReference);
+                if (transaction != null)
+                {
+                    _logger.LogError(
+                        "The transaction has already been processed. Provider reference: {Reference}, Current status: {CurrentStatus}, New status: {NewStatus}",
+                        providerReference, transaction.Status, newStatus);
+                    return;
+                }
+
+                var bankAccount = await _bankAccountRepository.GetBankAccountAsync(TransactionProvider.Flutterwave,
+                    data.ProviderVirtualAccountId);
+
+                if (bankAccount == null)
+                {
+                    _logger.LogCritical(
+                        "The transaction refers to an untracked bank account. " +
+                        "Provider Transaction Reference: {TransactionReference}, " +
+                        "Provider Bank Account Reference: {BankAccountReference}",
+                        providerReference, data.ProviderVirtualAccountId);
+                    return;
+                }
+
+                await _depositRepository.CreateTransactionAsync(new CreateDepositDto
+                {
+                    Amount                = data.Amount,
+                    ProviderFees          = data.Fees,
+                    TotalPlatformFees     = _priceCalculatorService.CalculateDepositFees(data.Amount),
+                    Status                = TransactionStatus.Successful,
+                    TransactionType       = TransactionType.Deposit,
+                    BankTransactionId     = data.BankTransactionReference,
+                    ProviderTransactionId = data.ProviderTransactionReference,
+                    InternalTransactionId = Guid.NewGuid(),
+                    Provider              = TransactionProvider.Flutterwave,
+                    BankAccountId         = bankAccount.Id,
+                    UserAccountId         = bankAccount.UserId
+                });
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e,
+                    "An error occurred while processing the webhook. Transaction Reference: {Reference}, Message: {Message}, StackTrace: {StackTrace}",
+                    providerReference, e.Message, e.StackTrace);
+
+                throw;
+            }
+        }
+    }
+}
---
+++ dngn-api-backend/Integrations/PaymentProviders/Flutterwave/Services/WebHooks/FlutterwaveOutwardBankTransferHook.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Threading.Tasks;
+using DngnApiBackend.Data.Models;
+using DngnApiBackend.Integrations.Models.Common;
+using DngnApiBackend.Integrations.Models.WebHooks;
+using DngnApiBackend.Integrations.Transfers;
+using DngnApiBackend.Services.Repositories;
+using DngnApiBackend.Services.WebhookServices;
+using Microsoft.Extensions.Logging;
+
+namespace DngnApiBackend.Integrations.PaymentProviders.Flutterwave.Services.WebHooks
+{
+    // todo: Implement customer notifications
+    public class FlutterwaveOutwardBankTransferHook : IOutwardBankTransferHook
+    {
+        private readonly ILogger<FlutterwaveOutwardBankTransferHook> _logger;
+        private readonly ITransferQueryProcessor _transferQueryProcessor;
+        private readonly IWithdrawalRepository _withdrawalRepository;
+
+
+        public FlutterwaveOutwardBankTransferHook(IWithdrawalRepository withdrawalRepository,
+            ITransferQueryProcessor transferQueryProcessor,
+            ILogger<FlutterwaveOutwardBankTransferHook> logger)
+        {
+            _withdrawalRepository   = withdrawalRepository;
+            _transferQueryProcessor = transferQueryProcessor;
+            _logger                 = logger;
+        }
+
+
+        public async Task ProcessHookAsync(JsonWebhookModel model)
+        {
+            if (model is not FlutterwaveJsonWebhookModel flwModel)
+            {
+                _logger.LogInformation(
+                    "OutwardBankTransferHook: Invalid attempt to process a non-flutterwave webhook model by a flutterwave webhook processor");
+
+                return;
+            }
+
+            var providerReference = flwModel.Id;
+            _logger.LogInformation("OutwardBankTransferHook: A notification is being processed. Reference: {Reference}",
+                providerReference);
+
+            // load the transaction data from flutterwave
+            var data = await _transferQueryProcessor.QueryTransferAsync(providerReference);
+            if (data?.ProviderTransactionReference == null)
+            {
+                _logger.LogInformation(
+                    "OutwardBankTransferHook: The provider failed to resolve the reference '{Reference}'",
+                    providerReference);
+
+                return;
+            }
+
+            try
+            {
+                var newStatus = data.Status;
+                var transaction =
+                    await _withdrawalRepository.GetTransactionByProviderReferenceAsync(
+                        TransactionProvider.Flutterwave, providerReference);
+
+                if (transaction == null)
+                {
+                    _logger.LogCritical(
+                        "The transaction does not exist. Provider reference: {Reference}, New status: {Status}",
+                        providerReference, newStatus);
+                    return;
+                }
+
+                await _withdrawalRepository.UpdateTransactionStatusAsync(transaction.Id, newStatus, data.TransferFees);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e,
+                    "An error occurred while processing the webhook. Message: {Message}, StackTrace: {StackTrace}",
+                    e.Message, e.StackTrace);
+
+                throw;
+            }
+        }
+    }
+}
