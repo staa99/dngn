bd1e8b9f84e880b631e2152b6a83fd05c9e36339
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-17 02:33:23 +0100

    Add user ID to bank account model

--- dngn-api-backend/Data/Models/BankAccount.cs
+++ dngn-api-backend/Data/Models/BankAccount.cs
@@ -7,8 +7,11 @@ namespace DngnApiBackend.Data.Models
     {
         public string AccountNumber { get; set; } = null!;
         public ObjectId? BankId { get; set; }
+        public ObjectId UserId { get; set; }
         public string AccountName { get; set; } = null!;
         public bool IsVirtual { get; set; }
-        public IDictionary<BankAccountMetaKey, string> Metadata { get; set; } = new Dictionary<BankAccountMetaKey, string>();
+
+        public IDictionary<BankAccountMetaKey, string> Metadata { get; set; } =
+            new Dictionary<BankAccountMetaKey, string>();
     }
 }
