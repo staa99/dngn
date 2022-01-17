776f3020877d2b46557f66d97fba3c6b3c3f1ab7
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-17 02:32:46 +0100

    Add virtual account reference prefix constant

--- dngn-api-backend/Integrations/Models/Common/Constants.cs
+++ dngn-api-backend/Integrations/Models/Common/Constants.cs
@@ -3,8 +3,10 @@
     public static class Constants
     {
         public const string CurrencyCode = "NGN";
-        
+
         // hard-coded. only one virtual account provider supported.
         public const string VirtualAccountProvider = nameof(Common.VirtualAccountProvider.Flutterwave);
+
+        public const string VirtualAccountReferencePrefix = "DNGN-DEPOSITACCT";
     }
 }
