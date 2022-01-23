0be0a0d0869c3fae682a6b295b6fb2efbf320bf5
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-23 15:14:27 +0100

    Trigger notification on successful transaction

--- dngn-api-backend/Services/Repositories/DepositRepository.cs
+++ dngn-api-backend/Services/Repositories/DepositRepository.cs
@@ -1,18 +1,25 @@
-﻿using DngnApiBackend.Data.Models;
+﻿using System.Threading.Tasks;
+using DngnApiBackend.Data.Models;
 using DngnApiBackend.Exceptions;
+using DngnApiBackend.Integrations.Notifications.Outgoing;
 using DngnApiBackend.Services.Dto;
 using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace DngnApiBackend.Services.Repositories
 {
     public class DepositRepository: BaseTransactionRepository<Deposit>, IDepositRepository
     {
+        private readonly IMinterNotificationSender _minterNotificationSender;
+        private readonly IUserAccountRepository _userAccountRepository;
         private readonly ILogger<DepositRepository> _logger;
 
-        public DepositRepository(IMongoDatabase database, ILogger<DepositRepository> logger) : base(database, DngnMongoSchema.DepositCollection)
+        public DepositRepository(IMinterNotificationSender minterNotificationSender, IUserAccountRepository userAccountRepository, IMongoDatabase database, ILogger<DepositRepository> logger) : base(database, DngnMongoSchema.DepositCollection)
         {
-            _logger = logger;
+            _minterNotificationSender   = minterNotificationSender;
+            _userAccountRepository = userAccountRepository;
+            _logger                     = logger;
         }
 
         protected override Deposit MapCreateTransactionDto<TDto>(TDto dto)
@@ -32,6 +39,28 @@ namespace DngnApiBackend.Services.Repositories
             var deposit = MapCreateDtoCommonFields(dto);
             deposit.RawWebhookPayload = depositDto.RawWebhookPayload;
             return deposit;
+        }
+
+        public override async Task<ObjectId> CreateTransactionAsync<TDto>(TDto dto)
+        {
+            var result = await base.CreateTransactionAsync(dto);
+            if (result == default || dto.Status != TransactionStatus.Successful)
+            {
+                return result;
+            }
+
+            var address = await _userAccountRepository.GetAddressAsync(dto.UserAccountId);
+            if (string.IsNullOrEmpty(address))
+            {
+                _logger.LogCritical("ORPHANED_DEPOSIT: Address for deposit does not exist");
+            }
+            else
+            {
+                await _minterNotificationSender.SendNotification(
+                    new MinterInstruction(dto.ProviderTransactionId!, address, dto.Amount, dto.TotalPlatformFees));
+            }
+
+            return result;
         }
     }
 }
