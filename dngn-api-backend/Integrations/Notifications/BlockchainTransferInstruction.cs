1fa7332b555e7ec4406d9ffbdd1b87f4af9b2fa6
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-28 08:24:36 +0100

    Create BlockchainTransferInstruction

---
+++ dngn-api-backend/Integrations/Notifications/BlockchainTransferInstruction.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DngnApiBackend.Integrations.Notifications
+{
+    public class BlockchainTransferInstruction
+    {
+        [JsonPropertyName("txHash")]
+        public string TransactionHash { get; set; } = null!;
+
+        [JsonPropertyName("address")]
+        public string ToAddress { get; set; } = null!;
+
+        [JsonPropertyName("amount")]
+        public long Amount { get; set; }
+    }
+}
