febe12ae0a602639b994fe682f1b2974108118b3
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-28 08:22:42 +0100

    Add blockchain transaction status and hash to dtos

--- dngn-api-backend/Services/Dto/CreateTransactionDto.cs
+++ dngn-api-backend/Services/Dto/CreateTransactionDto.cs
@@ -9,12 +9,14 @@ namespace DngnApiBackend.Services.Dto
     {
         public string? BankTransactionId { get; set; }
         public string? ProviderTransactionId { get; set; }
+        public string? BlockchainTransactionHash { get; set; }
         public Guid InternalTransactionId { get; set; }
         public long Amount { get; set; }
         public long ProviderFees { get; set; }
         public long TotalPlatformFees { get; set; }
         public TransactionProvider Provider { get; set; }
         public TransactionStatus Status { get; set; }
+        public TransactionBlockchainStatus BlockchainStatus { get; set; }
         public TransactionType TransactionType { get; set; }
         public ObjectId BankAccountId { get; set; }
         public ObjectId UserAccountId { get; set; }
--- dngn-api-backend/Services/Dto/TransactionDto.cs
+++ dngn-api-backend/Services/Dto/TransactionDto.cs
@@ -8,12 +8,14 @@ namespace DngnApiBackend.Services.Dto
     {
         public string? BankTransactionId { get; set; }
         public string ProviderTransactionId { get; set; } = null!;
+        public string? BlockchainTransactionHash { get; set; }
         public Guid InternalTransactionId { get; set; }
         public long Amount { get; set; }
         public long ProviderFees { get; set; }
         public long TotalPlatformFees { get; set; }
         public TransactionStatus Status { get; set; }
         public TransactionType TransactionType { get; set; }
+        public TransactionBlockchainStatus BlockchainStatus { get; set; }
         public ObjectId UserAccountId { get; set; }
         public ObjectId BankAccountId { get; set; }
         public DateTimeOffset DateCreated { get; set; }
