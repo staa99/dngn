1a5c9eb40e2b632fcd1444b686a760f4317925f6
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-29 09:48:30 +0100

    Create withdrawer entry point

---
+++ withdrawer/src/index.ts
@@ -0,0 +1,30 @@
+﻿import * as dotenv from 'dotenv'
+import Withdrawer from 'blockchain'
+import validators from 'validation'
+import { Messenger } from 'messaging'
+import { WithdrawerStore } from 'storage'
+
+dotenv.config()
+
+async function main() {
+  console.log('Starting DNGN Withdrawer')
+  validators.environment.checkEnvironment()
+  const withdrawerStore = new WithdrawerStore()
+  const withdrawer = new Withdrawer(withdrawerStore)
+  await withdrawer.connect()
+
+  const messenger = new Messenger(withdrawer)
+  await messenger.connect()
+  await messenger.startListening()
+
+  withdrawer.onWithdrawalTriggered(async (args) => {
+    await messenger.sendTransferNotification(args)
+  })
+
+  await withdrawer.startWithdrawalDetection()
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
