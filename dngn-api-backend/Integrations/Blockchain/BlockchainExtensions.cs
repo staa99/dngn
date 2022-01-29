d4ccb8e5dff6e056ab204a58f94eec73e02ecbf4
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-29 09:40:19 +0100

    Register blockchain services

---
+++ dngn-api-backend/Integrations/Blockchain/BlockchainExtensions.cs
@@ -0,0 +1,13 @@
+﻿using DngnApiBackend.Integrations.Blockchain.Incoming;
+using DngnApiBackend.Integrations.Blockchain.Outgoing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DngnApiBackend.Integrations.Blockchain
+{
+    public static class NotificationExtensions
+    {
+        public static IServiceCollection AddNotifications(this IServiceCollection services) =>
+            services.AddSingleton<IBlockchainNotifier, BlockchainNotifier>()
+                .AddHostedService<BlockchainSubscriber>();
+    }
+}
