9ab9704ec9e1df6e0b69157481e8526ab9f60201
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-29 09:47:48 +0100

    Implement environment validation

---
+++ withdrawer/src/validation/environment.ts
@@ -0,0 +1,17 @@
+export function ensureEnvironmentVariables(): void {
+  const requiredEnvKeys = [
+    'AMQP_URL',
+    'WITHDRAWER_ENCRYPTION_PRIVATE_KEY',
+    'COREAPI_ENCRYPTION_PUBLIC_KEY',
+    'WITHDRAWER_ACCOUNT_PRIVATE_KEY',
+    'BLOCKCHAIN_RPC_ENDPOINT',
+    'CONTRACT_ADDRESS',
+  ]
+
+  const unsetKeys = requiredEnvKeys.filter((key) => !process.env[key])
+  if (!unsetKeys.length) {
+    return
+  }
+
+  throw Error(`WITHDRAWER_LAUNCH_ERR: ${unsetKeys} not defined in environment variables`)
+}
