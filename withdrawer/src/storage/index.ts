6bf13cbfa0dee94a96ca0000d4dbf883e9a7a075
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-01-29 09:47:16 +0100

    Implement stub storage

---
+++ withdrawer/src/storage/index.ts
@@ -0,0 +1,16 @@
+﻿export class WithdrawerStore {
+  lastBlockNumber: number
+
+  constructor() {
+    this.lastBlockNumber = 0
+  }
+
+  async getLastBlockNumber(): Promise<number> {
+    return this.lastBlockNumber
+  }
+
+  async setLastBlockNumber(blockNumber: number): Promise<void> {
+    const lastBlockNumber = await this.getLastBlockNumber()
+    this.lastBlockNumber = Math.max(lastBlockNumber, blockNumber)
+  }
+}
