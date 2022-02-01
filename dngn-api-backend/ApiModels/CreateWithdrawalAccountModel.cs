790d5f908549f5dc4e5de8652bd6eb696b8057ce
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-02-01 23:16:27 +0100

    Cleanup CreateWithdrawalAccountModel

--- dngn-api-backend/ApiModels/CreateWithdrawalAccountModel.cs
+++ dngn-api-backend/ApiModels/CreateWithdrawalAccountModel.cs
@@ -8,9 +8,6 @@ namespace DngnApiBackend.ApiModels
         public string AccountNumber { get; set; } = null!;
         
         [Required]
-        public string AccountName { get; set; } = null!;
-        
-        [Required]
         public string BankId { get; set; } = null!;
     }
 }
